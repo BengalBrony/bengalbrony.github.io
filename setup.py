cc742352eeaf7db4afe686974e59f4ff2d93726a
bengalbrony <khanaf@slu.edu>
2020-08-26 12:00:10 -0500

    trying to make wheel + np work to no avail

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import find_packages, setup
-import numpy as np
+
 setup(
     name='test_py_lib',
     packages=find_packages(include=['test_py_lib']),
     version='0.1.0',
     description='My first Python library',
-    author='Me',
+    author='Ameera',
     license='MIT',
     install_requires=[],
     setup_requires=['pytest-runner'],
-    tests_require=['pytest==4.4.1'],
+    tests_require=['pytest==4.4.1', 'np'],
     test_suite='tests',
 )
