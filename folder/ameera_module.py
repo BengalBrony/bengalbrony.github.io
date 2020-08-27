1c554c9cd2be2dd85662d4a2cc8ece5ae2a48cb8
bengalbrony <khanaf@slu.edu>
2020-08-27 14:20:43 -0500

    you can't inherit modules from the parent directory...

---
+++ folder/ameera_module.py
@@ -0,0 +1,18 @@
+import os
+
+
+def funky():
+    print("hey adora")
+    print(os.path.basename(__file__))
+
+
+name = 'adora'
+
+
+class AmeeraTest:
+    user = "ameera"
+    role = "devops"
+    exp = 2
+
+    def shout(self, name):
+        print(name, "says hey catra")
---
+++ folder/ameera_script.py
@@ -0,0 +1,10 @@
+import ameera_module
+import sys
+sys.path.insert(0, '/bengalbrony.github.io')
+
+
+ameera_module.funky()
+
+ak = ameera_module.AmeeraTest()
+hey = ameera_module.name
+ak.shout(hey)
